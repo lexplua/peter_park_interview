041432692affe17b3b02f5fad11a9f4c27dd6876
lexplua <konstantin.lex@gmail.com>
2023-10-16 17:48:56 +0200

    extra logging and correct response

--- peter_park_interview/hello_world/text_ocr.py
+++ peter_park_interview/hello_world/text_ocr.py
@@ -1,38 +1,33 @@
 import base64
 import json
-
+import logging
 import boto3
 
 rekognition_client = boto3.client('rekognition')
 
+logging.basicConfig(level=logging.INFO)
 
 def detect_image_text(bytes):
     response = rekognition_client.detect_text(Image={'Bytes': bytes})
     return ' '.join(block['DetectedText'] for block in response['TextDetections'])
 
 def handler(event, context):
-    if 'image' not in event['body']:
-        lambda_response = {
-            'statusCode': 400,
-            'body': {
-                "Error": 'Missing `image` POST parameter',
-                "ErrorMessage": f"Missing `image` POST parameter, only {event['body']} are present"
-            }
-        }
-        return lambda_response
-    request_body = event['body']
+    logging.info("OCR handler started")
 
+    request_body = event['body']
+    logging.info("Request body received")
     try:
         request_body = json.loads(request_body)
         image = request_body['image']
+        logging.info("Encoded image retrieved from body")
         image_decoded = base64.b64decode(image)
-
+        logging.info("Image bytes decoded")
         text = detect_image_text(image_decoded)
         lambda_response = {
             'statusCode': 200,
-            'body': {
+            'body': json.dumps({
                 'text': text
-            }
+            })
         }
         return lambda_response
     except Exception as e:
