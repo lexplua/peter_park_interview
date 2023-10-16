f652e12db0772aef0df7348207d083aff40f73d6
lexplua <konstantin.lex@gmail.com>
2023-10-16 16:42:50 +0200

    Make post argument work

--- peter_park_interview/hello_world/text_ocr.py
+++ peter_park_interview/hello_world/text_ocr.py
@@ -1,4 +1,6 @@
 import base64
+import json
+
 import boto3
 
 rekognition_client = boto3.client('rekognition')
@@ -8,35 +10,37 @@ def detect_image_text(bytes):
     response = rekognition_client.detect_text(Image={'Bytes': bytes})
     return ' '.join(block['DetectedText'] for block in response['TextDetections'])
 
-
 def handler(event, context):
-    image = event.get('image', None)
-    if image is None:
+    if 'image' not in event['body']:
         lambda_response = {
             'statusCode': 400,
             'body': {
                 "Error": 'Missing `image` POST parameter',
-                "ErrorMessage": 'Missing `image` POST parameter'
+                "ErrorMessage": f"Missing `image` POST parameter, only {event['body']} are present"
             }
         }
         return lambda_response
-    else:
+    request_body = event['body']
+
+    try:
+        request_body = json.loads(request_body)
+        image = request_body['image']
         image_decoded = base64.b64decode(image)
-        try:
-            text = detect_image_text(image_decoded)
-            lambda_response = {
-                'statusCode': 200,
-                'body': {
-                    'text': text
-                }
+
+        text = detect_image_text(image_decoded)
+        lambda_response = {
+            'statusCode': 200,
+            'body': {
+                'text': text
             }
-            return lambda_response
-        except Exception as e:
-            lamda_response = {
-                'statusCode': 400,
-                'body': {
-                    'Error': "Exception during image processing",
-                    'ErrorMessage': str(e)
-                }
+        }
+        return lambda_response
+    except Exception as e:
+        lamda_response = {
+            'statusCode': 400,
+            'body': {
+                'Error': "Exception during image processing",
+                'ErrorMessage': str(e)
             }
-            return lamda_response
+        }
+        return lamda_response
